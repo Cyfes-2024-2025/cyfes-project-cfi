e58b39c8e4fb51ac2ecbceb1427c45c55edff930
Daem <daemondeal@gmail.com>
2024-05-27 14:36:48 +0200

    feat: added automation to skeleton

---
+++ first_pass_example/README.md
@@ -0,0 +1,12 @@
+# Comipler Pass Example
+
+
+## How to build
+
+```bash
+./build.sh
+cd example
+make
+```
+
+
---
+++ first_pass_example/example/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bar()
+{
+    printf("entered the bar ;)");
+    exit(0);
+}
+void** search(void** addr, void* value) __attribute__((noinline));
+void** search(void** addr, void* value)
+{
+    while(*addr != value) addr++;
+    return addr;
+}
+void foo() __attribute__((noinline));
+void foo()
+{
+    void** p = search((void**)&p, __builtin_return_address(0));
+    *p = (void *) bar;
+}
+int main()
+{
+    foo();
+    return 0;
+}
